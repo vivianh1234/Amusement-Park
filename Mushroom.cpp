4ed1c2e27ea59d2d233315d53d9fa0f39157457a
Vivian Huynh <hvivian@pdx.edu>
2021-03-11 23:48:23 -0800

    Mushroom coloring

--- Mushroom.cpp
+++ Mushroom.cpp
@@ -23,10 +23,43 @@ Mushroom::~Mushroom(void)
 //Initializer
 bool Mushroom::Initialize(void)
 {
-    glEnable(GL_TEXTURE_2D);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-    LoadTexture("mushroom_texture.tga");
+   /* ubyte* image_data;
+    int	    image_height, image_width;
 
+    // Load the image for the texture. The texture file has to be in
+    // a place where it will be found.
+    if (!(image_data = (ubyte*)tga_load("mushroom_texture.tga", &image_width,
+        &image_height, TGA_TRUECOLOR_24)))
+    {
+        fprintf(stderr, "Mushroom::Initialize: Couldn't load mushroom_texture.tga\n");
+        return false;
+    }
+
+    // This creates a texture object and binds it, so the next few operations
+    // apply to this texture.
+    glGenTextures(1, &texture_obj);
+    glBindTexture(GL_TEXTURE_2D, texture_obj);
+
+    // This sets a parameter for how the texture is loaded and interpreted.
+    // basically, it says that the data is packed tightly in the image array.
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // This sets up the texture with high quality filtering. First it builds
+    // mipmaps from the image data, then it sets the filtering parameters
+    // and the wrapping parameters. We want the grass to be repeated over the
+    // ground.
+    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, image_width, image_height,
+        GL_RGB, GL_UNSIGNED_BYTE, image_data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+        GL_NEAREST_MIPMAP_LINEAR);
+
+    // This says what to do with the texture. Modulate will multiply the
+    // texture by the underlying color.
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    */
 
 	initialized = true;
 	return true;
@@ -37,24 +70,25 @@ void Mushroom::Draw(void)
 {
     //Draw cylinder for base
     glPushMatrix();
-	glColor3f(244, 235, 186);
+    glColor3f(0.99, 0.99, 0.92);
 	GLUquadric* cylinder = gluNewQuadric();
 	gluCylinder(cylinder, 1, 0.8, 3, 15, 15);
     glPopMatrix();
 
     //Draw circle (base for top of mushroom)
     glPushMatrix();
-    glColor3f(255, 0, 0);
+    glColor3f(1, 0, 0);
     glTranslatef(0, 0, 3);
     DrawCircle(0, 0, 2, 8);
     glPopMatrix();
 
     //Draw half-sphere for top
     glPushMatrix();
-    glColor3f(255, 0, 0);
+    glColor3f(1, 0, 0);
     glTranslatef(0, 0, 3);
     glRotatef(90, 1, 0, 0);
     DrawHalfSphere(5.0f, 5.0f, 2);
+    //glCallList(display_list);
 	glPopMatrix();
     
 }
@@ -64,7 +98,7 @@ void Mushroom::Draw(float base_height, float base_rad, float top_rad, float x, f
 {
     //Draw cylinder for base
     glPushMatrix();
-    glColor3f(244, 235, 186);
+    glColor3f(0.99, 0.99, 0.92);
     glTranslatef(x, y, z);
     GLUquadric* cylinder = gluNewQuadric();
     gluCylinder(cylinder, base_rad, base_rad * 0.75, base_height, 10, 10);
@@ -72,17 +106,18 @@ void Mushroom::Draw(float base_height, float base_rad, float top_rad, float x, f
 
     //Draw circle (base for top of mushroom)
     glPushMatrix();
-    glColor3f(255, 0, 0);
+    glColor3f(1, 0, 0);
     glTranslatef(x, y, z + base_height);
     DrawCircle(0, 0, top_rad, 8);
     glPopMatrix();
 
     //Draw half-sphere for top
     glPushMatrix();
-    glColor3f(255, 0, 0);
+    glColor3f(1, 0, 0);
     glTranslatef(x, y, z + base_height);
     glRotatef(90, 1, 0, 0);
     DrawHalfSphere(5, 5, top_rad);
+    //glCallList(display_list);
     glPopMatrix();
 
 }
@@ -106,16 +141,40 @@ void Mushroom::DrawHalfSphere(int scaley, int scalex, GLfloat r) {
         }
     }
    
+  /*  //Texturing
+    // Now do the geometry. Create the display list.
+    display_list = glGenLists(1);
+    glNewList(display_list, GL_COMPILE);
+    // Use white, because the texture supplies the color.
+    glColor3f(0, 255, 0);
+
+    // The surface normal is up for the ground.
+    //glNormal3f(0.0, 0.0, 1.0);
+
+    // Turn on texturing and bind the texture.
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture_obj);
+
+    //glRotatef(90, 1, 0, 0);
+    */
+
     glBegin(GL_QUADS);
     for (i = 0; i < scalex - 1; ++i) {
         for (j = 0; j < scaley; ++j) {
-            glTexCoord2f(0, 1); glVertex3fv(v[i * scaley + j]);
+           /* glTexCoord2f(0, 1); glVertex3fv(v[i * scaley + j]);
             glTexCoord2f(0, 0); glVertex3fv(v[i * scaley + (j + 1) % scaley]);
             glTexCoord2f(1, 0); glVertex3fv(v[(i + 1) * scaley + (j + 1) % scaley]);
-            glTexCoord2f(1, 1); glVertex3fv(v[(i + 1) * scaley + j]);
+            glTexCoord2f(1, 1); glVertex3fv(v[(i + 1) * scaley + j]);*/
+            glVertex3fv(v[i * scaley + j]);
+            glVertex3fv(v[i * scaley + (j + 1) % scaley]);
+            glVertex3fv(v[(i + 1) * scaley + (j + 1) % scaley]);
+            glVertex3fv(v[(i + 1) * scaley + j]);
         }
     }
     glEnd();
+
+    //glDisable(GL_TEXTURE_2D);
+    //glEndList();
 }
 
 //Draws a red circle
@@ -130,7 +189,6 @@ void Mushroom::DrawCircle(float cx, float cy, float r, int num_segments)
     float x = r;//we start at angle = 0 
     float y = 0;
 
-    glColor3f(255, 0, 0);
     glBegin(GL_LINE_LOOP);
     for (int ii = 0; ii < num_segments; ii++)
     {
@@ -143,7 +201,6 @@ void Mushroom::DrawCircle(float cx, float cy, float r, int num_segments)
     }
     glEnd();
 
-    glColor3f(255, 0, 0);
     glBegin(GL_POLYGON);
     for (int ii = 0; ii < num_segments; ii++)
     {
@@ -155,64 +212,6 @@ void Mushroom::DrawCircle(float cx, float cy, float r, int num_segments)
         y = s * t + c * y;
     }
     glEnd();
-}
-
-//Load Texture
-void Mushroom::LoadTexture(const char* filename)
-{
-    TargaImage* image = TargaImage::Load_Image((char*)filename);
-    if (!image)
-    {
-        std::cerr << "Failed to load texture:  " << filename << std::endl;
-        return;
-    }
-
-    // reverse the row order
-    TargaImage* reversedImage = image->Reverse_Rows();
-    delete image;
-    image = reversedImage;
-
-    if (!ResizeImage(image))
-    {
-        std::cerr << "Failed to resize texture." << std::endl;
-        return;
-    }
-
-    glGenTextures(1, &textureId);
-    glBindTexture(GL_TEXTURE_2D, textureId);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image->width, image->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image->data);
-}
-
-//Resize Image
-bool Mushroom::ResizeImage(TargaImage* image)
-{
-    int newWidth = pow(2.0, (int)ceil(log((float)image->width) / log(2.f)));
-    int newHeight = pow(2.0, (int)ceil(log((float)image->width) / log(2.f)));
-
-    newWidth = max(64, newWidth);
-    newHeight = max(64, newHeight);
-
-    if (newWidth != image->width && newHeight != image->height)
-    {
-        unsigned char* scaledData = new unsigned char[newWidth * newHeight * 4];
-        if (gluScaleImage(GL_RGBA, image->width, image->height, GL_UNSIGNED_BYTE, image->data, newWidth, newHeight, GL_UNSIGNED_BYTE, scaledData) != 0)
-        {
-            delete[] scaledData;
-            return false;
-        }// if
-
-        delete image->data;
-        image->data = scaledData;
-        image->width = newWidth;
-        image->height = newHeight;
-    }// if
-
-    return true;
 }
 
 
